c1d83cc2ba9bdf645acfddbbca5bc4ed48d9ed7f
Marcelo F Andrade <mfandrade@gmail.com>
2013-08-02 01:30:39 -0300

    Receive the intent and Display the message.

--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -3,10 +3,12 @@ package com.example.myfirstapp;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -21,6 +23,18 @@ public class DisplayMessageActivity extends Activity {
 			// Show the Up button in the action bar.
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+		
+		// Get the message from the intent.
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		// Create the text view.
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+		
+		// Set the textView as activity layout.
+		setContentView(textView);
 	}
 
 	/**
