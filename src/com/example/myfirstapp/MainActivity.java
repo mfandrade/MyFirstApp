22aa260f9079cdb5c3f27cd055959fa499444da1
Marcelo F Andrade <mfandrade@gmail.com>
2013-08-01 23:35:53 -0300

    Start the second activity.

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends Activity {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
